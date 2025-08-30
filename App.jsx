95c4e7341c26713d7540e01f409502b805f49d4a
egoroffr <egoroff.r@gmail.com>
2025-08-30 22:21:47 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '40px' }}>
+      <h1>Моё портфолио</h1>
+      <p>Добро пожаловать! Здесь будут мои фотографии и проекты.</p>
+    </div>
+  )
+}
+
+export default App
